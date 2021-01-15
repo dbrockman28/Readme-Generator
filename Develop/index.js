1513d426b3b859618ca32a0abf2e155c01caf9f4
Rowdy Goat <dbrockman28@gmail.com>
2021-01-14 20:04:37 -0600

    finished project

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,147 @@
-const inquirer = require("inuirer");
-const fs = require("fs");
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require('fs');
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function promptUser() {
+    return inquirer.prompt([
+    {
+        type:"input",
+        name: "username",
+        message: "What is your Github username?",
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+        type:"input",
+        name: "project",
+        message: "What is your project name?",
+    },
+    {
+        type:"input",
+        name:"description",
+        message:"Please write a short description of your project?",
+    },
+    {
 
-// Function call to initialize app
-init();
+        type:"list",
+        name:"license",
+        message: "What kind of license should your project have? User can choose from list of items?",
+        choices:[
+            "MIT",
+            "Mozilla",
+            "Zlib",
+        ]
+    },
+    {
+        type:"input",
+        name:"installation",
+        message:"What command should be run to install dependencies?",
+        default: 'npm i',
+
+    },
+
+    {
+        type:"input",
+        name:"test",
+        message:"What command should be run to run tests?",
+        default: 'npm test'
+
+    },
+    {
+        type:"input",
+        name:"additionalInfo",
+        message:"What does the user need to know about using the repo?",
+    },
+    {
+        type:"input",
+        name:"contribute",
+        message:"What does the user need to know about contributing to the repo?",
+
+    },
+
+]);
+}
+
+function generateReadMe (response,answers,link) {
+ return `
+ ##  ${answers.project}
+ ![License](${link})
+ ## Description
+  ${answers.description}
+ 
+ ## Table of Contents 
+ 
+ 
+ * [Installation](#installation)
+ 
+ * [Usage](#usage)
+ 
+ * [License](#license)
+ 
+ * [Contributing](#contributing)
+ 
+ * [Tests](#tests)
+ 
+ * [Questions](#questions)
+ 
+ ## Installation
+ ${answers.installation
+}
+ 
+ ## Usage
+ ${answers.additionalInfo}
+ 
+ 
+ ## License
+ ${answers.license}
+ ## Contributing
+ ${answers.contribute}
+ 
+ 
+ ## Tests
+ ${answers.test}
+ 
+ 
+ ## Questions
+ 
+ <img src="${response.avatar_url}" alt="avatar" style="border-radius: 16px" width="30" />
+ 
+ If you have any questions about the repo, open an issue or contact [${response.login}](${response.html_url}).
+ `;
+
+
+
+}
+
+promptUser()
+.then(function(answers){
+
+    if (answers.license=== "MIT") {
+        link = "https://img.shields.io/badge/License-MIT-yellow.svg"
+    }
+
+    if (answers.license === "Mozilla") {
+        link = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+    }
+    if (answers.license === "Zlib") {
+
+        link = "[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)"
+    }
+    const queryUrl = `https://api.github.com/users/${answers.username}`;
+
+    axios.get(queryUrl).then(function(res) {
+        const response = res.data;
+        const readMe = generateReadMe (response,answers,link);
+    return writeFileAsync ("template.md", readMe);
+      
+          
+    });
+    
+})
+.then(function() {
+    console.log("Successfully wrote to template.md");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
---
+++ Develop/template.md
@@ -0,0 +1,44 @@
+
+ ##  Taskinator
+ ![License]([![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0))
+ ## Description
+  Creates tasks in a calendar layout
+ 
+ ## Table of Contents 
+ 
+ 
+ * [Installation](#installation)
+ 
+ * [Usage](#usage)
+ 
+ * [License](#license)
+ 
+ * [Contributing](#contributing)
+ 
+ * [Tests](#tests)
+ 
+ * [Questions](#questions)
+ 
+ ## Installation
+ npm i
+ 
+ ## Usage
+ 
+ 
+ 
+ ## License
+ Mozilla
+ ## Contributing
+ 
+ 
+ 
+ ## Tests
+ npm test
+ 
+ 
+ ## Questions
+ 
+ <img src="https://avatars0.githubusercontent.com/u/46579223?v=4" alt="avatar" style="border-radius: 16px" width="30" />
+ 
+ If you have any questions about the repo, open an issue or contact [dbrockman28](https://github.com/dbrockman28).
+ 
